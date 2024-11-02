18ba9ad5dbcbce154d7bc52936b4c513f6861cae
abdullahelmasry <abdallhking3040@gmail.com>
2024-11-02 18:06:16 +0200

    hi

--- main_api/main_api.ino
+++ main_api/main_api.ino
@@ -17,7 +17,9 @@ void setup() {
 }
 
 void loop() {
-  system();
+  system_box1();
+  system_box2();
+  system_box3();
 }
 
 // Flags that will control the system workflow
@@ -27,10 +29,9 @@ pickup_done
 item_loaded
 */
 
-void system(){
+void system_box1(){
  
   // check item in the conveyor pickup position
-
   conveyor()  // step 1
 
   if (conveyor_done){  // flag from conveyor.h
@@ -42,12 +43,80 @@ void system(){
   if (pickup_done){   // flag from pickup.h
       moveTo(5,5); // step 3
   }
+
+  delay(1000);
+
   if (item_loaded){   // flag from moveXZ.h
     pickUp(); // step 4  >> load item to the shelf
   }
+
+  delay(1000);
 
   if (pickup_done){
     moveTo(0,0); // step 5 last step, the arm will go back to the start postion 0,0
   }
 
+  delay(1000);
+}
+
+void system_box2(){
+ 
+  // check item in the conveyor pickup position
+  conveyor()  // step 1
+
+  if (conveyor_done){  // flag from conveyor.h
+    pickUp(); // step 2
+  }
+
+  delay(1000);
+
+  if (pickup_done){   // flag from pickup.h
+      moveTo(5,4); // step 3         /////////////////////////// adjustable
+  }
+
+  delay(1000);
+
+  if (item_loaded){   // flag from moveXZ.h
+    pickUp(); // step 4  >> load item to the shelf
+  }
+
+  delay(1000);
+
+  if (pickup_done){
+    moveTo(0,0); // step 5 last step, the arm will go back to the start postion 0,0
+  }
+
+  delay(1000);
+}
+
+void system_box3(){
+ 
+  // check item in the conveyor pickup position
+  conveyor()  // step 1
+
+  if (conveyor_done){  // flag from conveyor.h
+    pickUp(); // step 2
+  }
+
+  delay(1000);
+
+  if (pickup_done){   // flag from pickup.h
+      moveTo(5,3); // step 3   /////////////////////////// adjustable
+  }
+
+  delay(1000);
+
+  if (item_loaded){   // flag from moveXZ.h
+    pickUp(); // step 4  >> load item to the shelf
+  }
+
+  delay(1000);
+
+  if (pickup_done){
+    moveTo(0,0); // step 5 last step, the arm will go back to the start postion 0,0
+  }
+
+  delay(1000);
+
+  while(true);
 }
